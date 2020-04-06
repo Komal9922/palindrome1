d63d327e421a8aebe8ab8e6ad761eb2fec5ce4ff
Komal9922 <48016521+Komal9922@users.noreply.github.com>
2020-04-06 11:09:06 +0530

    Create Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,8 @@
+number = int(input("enter the no");
+string = str(number);
+rev_string = string [::-1];
+print("revesrd number",rev_string);
+if string == rev_string
+print("the number is palindrome");
+else:
+print("the number is not palindrome");
